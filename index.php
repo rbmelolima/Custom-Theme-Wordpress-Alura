35fd1ccd61f476a866e829b36c8683c8b53c8b4f
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-12-08 18:41:24 -0300

    criando pasta de componentes

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
-  require_once 'header.php';
+  require_once 'components/header.php';
 ?>
 
 <h1>Hello World</h1>
 
 <?php 
-  require_once 'footer.php';
+  require_once 'components/footer.php';
 ?>
