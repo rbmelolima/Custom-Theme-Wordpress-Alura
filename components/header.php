545c8782b2ef8d66d6f32b338f9902ebd942a2dd
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-12-08 19:30:31 -0300

    estilo do header e do footer

--- components/header.php
+++ components/header.php
@@ -5,7 +5,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="<?= get_template_directory_uri() . '/styles/reset.css' ?>">
+
+  <link rel="stylesheet" href="<?= get_template_directory_uri() . '/styles/global.css' ?>">
+
   <title>
     <?php bloginfo('name') ?>
   </title>
@@ -13,13 +17,14 @@
   <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
 
-  <?php
-  the_custom_logo();
+  <nav class="header-navbar">
+    <?php
+    the_custom_logo();
 
-
-  wp_nav_menu(array(
-    'menu' => 'menu-navigation'
-  ));
-  ?>
+    wp_nav_menu(array(
+      'menu' => 'menu-navigation',      
+    ));
+    ?>
+  </nav>
