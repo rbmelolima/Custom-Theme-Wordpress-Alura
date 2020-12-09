385609f4bc513d24d2e1de86a3ee118d1249df2e
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-12-08 22:32:11 -0300

    implementando filtro de destinos

--- page-destinos.php
+++ page-destinos.php
@@ -1,10 +1,44 @@
 <?php require_once 'components/header.php'; ?>
 
-<main>
+<main id="page-destiny">
+  <section>
+    <form action="#">
+      <h2>Conheça nossos destinos</h2>
+      <div class="group">
+        <select name="countries" id="countries">
+          <option value="" disabled selected>Selecione um país</option>
+          <?php 
+          $countries = get_terms(array('taxonomy' => 'country'));
+
+          foreach($countries as $country): ?>
+
+          <option <?= !empty($_GET['countries']) && $_GET['countries'] === $country->name ? 'selected' : '' ?>
+            value="<?= $country->name ?>">
+            <?= $country->name ?>
+          </option>
+
+          <?php endforeach; ?>
+        </select>
+        <button type=" submit" value="search">Pesquisar</button>
+      </div>
+    </form>
+  </section>
 
   <section class="grid-destiny">
     <?php
-    $args = array('post_type' => 'destiny');
+    if(!empty($_GET['countries'])){
+      $selectedCountry = array(array(
+        'taxonomy' => 'country',
+        'field' => 'name',
+        'terms' => $_GET['countries']
+      ));
+    }
+    
+    $args = array(
+      'post_type' => 'destiny',
+      'tax_query' => !empty($_GET['countries']) ? $selectedCountry : '',
+    );
+    
     $query = new WP_Query($args);
 
     if ($query->have_posts()) :
