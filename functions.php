2213cb30c81c03ead41eb908e037d72489cbc986
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-12-08 23:07:17 -0300

    registrando banner

--- functions.php
+++ functions.php
@@ -56,9 +56,55 @@ function alura_register_custom_post_banner(){
   );
 }
 
+//Note: 'cp' is custom post
+
+function alura_register_metabox_cp_banner(){
+  add_meta_box(
+    'ai_register_metabox',
+    'Texto para a página inicial',
+    'alura_callback_cp_banner',
+    'banners'
+  );
+}
+
+function alura_callback_cp_banner($post){
+  $text_home_1 = get_post_meta($post->id, '_text_home_1', true );
+  $text_home_2 = get_post_meta($post->id, '_text_home_2', true );
+
+  ?>
+<label for="text_home_1">Texto 1</label>
+<input type="text" name="text_home_1" value="<?= $text_home_1 ?>" style="width: 100%" />
+<br>
+<br>
+<br>
+
+<label for="text_home_2">Texto 2</label>
+<input type="text" name="text_home_2" value="<?= $text_home_2 ?>" style="width: 100%" />
+<?php
+}
+
+function alura_save_cp_metabox_banner($post_id){
+  foreach($_POST as $key=>$value){
+    if($key !== 'text_home_1' && $key !== 'text_home_2'){
+      continue;
+    }
+
+    update_post_meta(
+      $post_id,
+      '_'.$key,
+      $_POST[$key]
+    );
+  }
+}
+
 //Registrando as funções
 add_action('init', 'alura_register_menu');
 add_action('init', 'alura_register_custom_post');
 add_action('init', 'alura_register_taxonomy');
 add_action('init', 'alura_register_custom_post_banner');
+
+add_action('add_meta_boxes', 'alura_register_metabox_cp_banner');
+
 add_action('after_setup_theme', 'alura_add_resources');
+
+add_action('save_post', 'alura_save_cp_metabox_banner');
